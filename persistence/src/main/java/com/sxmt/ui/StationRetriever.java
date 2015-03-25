6ecd1f2cce3d635d7e41cb0a6885a744509875f3
Alex Vertlieb <alex.vertlieb@cubrc.org>
2015-03-25 16:51:17 -0400

    oops

--- persistence/src/main/java/com/sxmt/ui/StationRetriever.java
+++ persistence/src/main/java/com/sxmt/ui/StationRetriever.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class StationRetriever
 {
-	private static final Logger LOG = LoggerFactory.getLogger(VideoRetriever.class);
+	private static final Logger LOG = LoggerFactory.getLogger(StationRetriever.class);
 
 	public static List<Station> getStations() throws SQLException
 	{
